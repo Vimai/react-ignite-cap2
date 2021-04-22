4008030df58177049782770bc528f5043055bec6
Vinicius Imai <vinicius.imai@gmail.com>
2021-04-22 20:29:30 -0300

    feat: img and global colors

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,18 @@
+import { GlobalStyle } from './styles/global'
+import styled from 'styled-components'
+
+
+const Title = styled.h1`
+  color: #8257e6;
+  font-size: 64px;
+`
+
 export function App() {
   return (
     <div className="App">
       <h1>Hello World</h1>
+      <Title>Hello World</Title>
+      <GlobalStyle />
     </div>
   );
 }
